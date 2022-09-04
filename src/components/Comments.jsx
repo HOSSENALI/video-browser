3ae4b681ff8ad616a277b5d8e1a40b2c4ae92069
HOSSENALI <hossen11@cse.pstu.ac.bd>
2022-09-05 01:05:49 +0200

    Redirect to comment section after login

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,5 +1,5 @@
-import { Button } from "@mui/material";
-import React, { useState } from "react";
+import {Button} from "@mui/material";
+import React, {useState} from "react";
 import styled from "styled-components";
 
 const Container = styled.div``;
@@ -23,8 +23,8 @@ const Avatar = styled.img`
 
 const Input = styled.input`
   border: none;
-  border-bottom: 1px solid ${({ theme }) => theme.soft};
-  color: ${({ theme }) => theme.text};
+  border-bottom: 1px solid ${({theme}) => theme.soft};
+  color: ${({theme}) => theme.text};
   background-color: transparent;
   outline: none;
   padding: 5px;
@@ -34,7 +34,7 @@ const Details = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
-  color: ${({ theme }) => theme.text};
+  color: ${({theme}) => theme.text};
 `;
 const Name = styled.span`
   font-size: 13px;
@@ -44,7 +44,7 @@ const Name = styled.span`
 const Date = styled.span`
   font-size: 12px;
   font-weight: 400;
-  color: ${({ theme }) => theme.textSoft};
+  color: ${({theme}) => theme.textSoft};
   margin-left: 5px;
 `;
 
@@ -52,66 +52,68 @@ const Text = styled.span`
   font-size: 14px;
 `;
 const Comments = () => {
-  const [comments, setComments] = useState([]);
-  const [comment, setComment] = useState("");
-  const [singleComment, setSingleComment] = useState({});
+    const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState("");
+    const [singleComment, setSingleComment] = useState({});
 
-  const userData = JSON.parse(localStorage.getItem("userData")) || undefined;
+    const userData = JSON.parse(localStorage.getItem("userData")) || undefined;
 
-  const handleComment = (e) => {
-    setComment(e.target.value);
-    createSingleComment();
-  };
-
-  const createSingleComment = () => {
-    const SingleComment = {
-      details: comment,
-      user: userData.user.email,
+    const handleComment = (e) => {
+        setComment(e.target.value);
+        createSingleComment();
     };
-    setSingleComment(SingleComment);
-  };
 
-  const addComment = () => {
-    let newComments = comments.slice();
-    newComments.unshift(singleComment);
-    setComments(newComments);
-    setComment("");
-  };
+    const handleInputClick = (e) => {
+        if (userData == undefined) {
+            window.location.href = "/auth?redirectTo=" + window.location.href;
+        }
+    };
 
-  // console.log("my comment", comments);
-  // console.log("comment", comment);
-  return (
-    <Container>
-      <NewComment>
-        <Avatar src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
-        {/* <Input placeholder="Add a comment..." onCreateTodo={todo => addTodo(todo)}/> */}
-        <Input
-          placeholder=" Add a comment....."
-          value={comment}
-          onChange={(e) => handleComment(e)}
-        />
-        <Button onClick={(e) => addComment()}>Add</Button>
-      </NewComment>
-      {/* {comments.map((comment) => (
-          <div>{comment.user}</div>
-        ))} */}
-      {/* <Comment comments={comments} /> */}
+    const createSingleComment = () => {
+        const SingleComment = {
+            details: comment,
+            user: userData.user.displayName,
+        };
+        setSingleComment(SingleComment);
+    };
 
-      {comments.map((comment) => (
-        <Container2>
-          {/* <Comment comments={comments} /> */}
-          <Avatar src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
+    const addComment = () => {
+        let newComments = comments.slice();
+        newComments.unshift(singleComment);
+        setComments(newComments);
+        setComment("");
+    };
 
-          <Details>
-            <Name>
-              {comment.user} <Date>1 day ago</Date>
-            </Name>
-            <Text>{comment.details}</Text>
-          </Details>
-        </Container2>
-      ))}
-    </Container>
-  );
+
+    return (
+        <Container>
+            <NewComment>
+                <Avatar
+                    src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo"/>
+                <Input
+                    placeholder=" Add a comment....."
+                    value={comment}
+                    onClick={() => handleInputClick()}
+                    onChange={(e) => handleComment(e)}
+                />
+                <Button onClick={(e) => addComment()}>Add</Button>
+            </NewComment>
+
+            {comments.map((comment) => (
+                <Container2>
+                    <Avatar
+                        src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo"/>
+
+                    <Details>
+                        <Name>
+                            {comment.user} <Date>1 day ago</Date>
+                        </Name>
+                        <Text>{comment.details}</Text>
+                    </Details>
+                </Container2>
+            ))}
+        </Container>
+    );
 };
 
 export default Comments;
--- src/components/utils/firebase/firebase.utils.js
+++ src/components/utils/firebase/firebase.utils.js
@@ -1,33 +1,33 @@
-import { initializeApp } from "firebase/app";
+import {initializeApp} from "firebase/app";
 import {
-  getAuth,
-  signInWithRedirect,
-  signInWithPopup,
-  GoogleAuthProvider,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  onAuthStateChanged,
-  updateProfile
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+    updateProfile
 } from "firebase/auth";
 import {
-  getFirestore,
-  doc,
-  getDoc,
-  setDoc,
-  collection,
-  writeBatch,
-  query,
-  getDocs,
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc,
+    collection,
+    writeBatch,
+    query,
+    getDocs,
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAvcE7EQozwOP1H9CGwzmIPNNt-8STT8cE",
-  authDomain: "my-ecommerce-b0dd8.firebaseapp.com",
-  projectId: "my-ecommerce-b0dd8",
-  storageBucket: "my-ecommerce-b0dd8.appspot.com",
-  messagingSenderId: "576378124915",
-  appId: "1:576378124915:web:9cb4dd769dfa09518e8806",
+    apiKey: "AIzaSyAvcE7EQozwOP1H9CGwzmIPNNt-8STT8cE",
+    authDomain: "my-ecommerce-b0dd8.firebaseapp.com",
+    projectId: "my-ecommerce-b0dd8",
+    storageBucket: "my-ecommerce-b0dd8.appspot.com",
+    messagingSenderId: "576378124915",
+    appId: "1:576378124915:web:9cb4dd769dfa09518e8806",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
@@ -35,82 +35,86 @@ const firebaseApp = initializeApp(firebaseConfig);
 const googleProvider = new GoogleAuthProvider();
 
 googleProvider.setCustomParameters({
-  prompt: "select_account",
+    prompt: "select_account",
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () =>
-  signInWithPopup(auth, googleProvider);
+    signInWithPopup(auth, googleProvider);
 export const signInWithGoogleRedirect = () =>
-  signInWithRedirect(auth, googleProvider);
+    signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
 
 export const createUserDocumentFromAuth = async (
-  userAuth,
-  additionalInformation = {}
+    userAuth,
+    additionalInformation = {}
 ) => {
-  if (!userAuth) return;
+    if (!userAuth) return;
 
-  const userDocRef = doc(db, "users", userAuth.uid);
+    const userDocRef = doc(db, "users", userAuth.uid);
 
-  const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot);
-  if (!userSnapshot.exists()) {
-    const { displayName, email } = userAuth;
-    // console.log(displayName);
-    const createdAt = new Date();
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    if (!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        // console.log(displayName);
+        const createdAt = new Date();
 
-    try {
-      await setDoc(userDocRef, {
-        displayName,
-        email,
-        createdAt,
-        ...additionalInformation,
-      });
-    } catch (error) {
-      console.log("error creating the user", error.message);
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+                ...additionalInformation,
+            });
+        } catch (error) {
+            console.log("error creating the user", error.message);
+        }
     }
-  }
 
-  return userDocRef;
+    return userDocRef;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password, displayName) => {
-  if (!email || !password) return;
-  return await createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredentials) => {
-      const user = userCredentials.user;
-      console.log("user", user);
-      updateProfile(auth.currentUser, {
-          displayName: displayName
-      }).then((s) => {
-        window.location.href = "/"
-      }).catch((error) => {
-          alert(error.message);
-      })
-  });
+    if (!email || !password) return;
+    return await createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredentials) => {
+            const user = userCredentials.user;
+            console.log("user", user);
+            updateProfile(auth.currentUser, {
+                displayName: displayName
+            }).then((s) => {
+                window.location.href = "/"
+            }).catch((error) => {
+                alert(error.message);
+            })
+        });
 };
 
-export const signInAuthUserWithEmailAndPassword = async (email, password) => {
-  if (!email || !password) return;
-  try {
-    const loginResult = await signInWithEmailAndPassword(auth, email, password);
-    localStorage.setItem("userData", JSON.stringify(loginResult));
-    window.location.href = "/";
-  } catch (err) {
-    console.error(err);
-    alert("Username and password does not match.");
-  }
+export const signInAuthUserWithEmailAndPassword = async (email, password, redirectUrl = null) => {
+    if (!email || !password) return;
+    try {
+        const loginResult = await signInWithEmailAndPassword(auth, email, password);
+        localStorage.setItem("userData", JSON.stringify(loginResult));
+        if(redirectUrl == null){
+            window.location.href = "/";
+        }else {
+            window.location.href = redirectUrl;
+        }
+    } catch (err) {
+        console.error(err);
+        alert("Username and password does not match.");
+    }
 };
 
 export const signOutUser = async () => {
-  const signOutResult = await signOut(auth);
-  console.log(auth);
-  localStorage.removeItem("userData");
-  window.location.href = "/auth";
+    const signOutResult = await signOut(auth);
+    console.log(auth);
+    localStorage.removeItem("userData");
+    window.location.href = "/auth";
 };
 
 export const onAuthStateChangedListener = (callback) =>
-  onAuthStateChanged(auth, callback);
+    onAuthStateChanged(auth, callback);
